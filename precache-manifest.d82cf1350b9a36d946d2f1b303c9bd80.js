4b72af3f6e60416bd96288bf557d8f46ddde7b08
André <andre@furnes.org>
2020-09-10 18:03:53 +0200

    Updates

---
+++ precache-manifest.d82cf1350b9a36d946d2f1b303c9bd80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72d6bcb47d221a81334b13204bbcace0",
+    "url": "/infoskjerm/index.html"
+  },
+  {
+    "revision": "990367cc6cd3e5451968",
+    "url": "/infoskjerm/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "426363e7c0d86f593f0d",
+    "url": "/infoskjerm/static/js/2.fb5b5def.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/infoskjerm/static/js/2.fb5b5def.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "990367cc6cd3e5451968",
+    "url": "/infoskjerm/static/js/main.2755ea35.chunk.js"
+  },
+  {
+    "revision": "3e63cd5e5c68c684204f",
+    "url": "/infoskjerm/static/js/runtime-main.e679cb49.js"
+  }
+]);
